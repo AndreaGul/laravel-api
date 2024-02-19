790f5a102efbecbcc25bf6c2bcd2d2260f571384
AndreaGul <andreagulino99@gmail.com>
2024-02-19 21:45:33 +0100

    feat cambio risposta json dell'index del project controller in api

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -21,7 +21,7 @@ class ProjectController extends Controller
 
         return response()->json(
             ['status'=>true,
-            'data'=>$projects]
+            'results'=>$projects]
         );
     }
 
